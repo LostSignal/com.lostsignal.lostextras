60d6cd3831cd37504e35c5c0eff7a845c07d82f0
Brian Gish <gish.brian@gmail.com>
2022-01-04 22:05:51 -0800

    Adding new UserInfo and Matchmaking managers to playfab.

---
+++ Runtime/PlayFab/PlayFabMatchmakingManager.cs
@@ -0,0 +1,102 @@
+//-----------------------------------------------------------------------
+// <copyright file="PlayFabMatchmakingManager.cs" company="Full Circle Games">
+//     Copyright (c) Full Circle Games. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+#if UNITY
+
+namespace Lost.PlayFab
+{
+    using Networking;
+    using global::PlayFab.ClientModels;
+    using Lost.CloudFunctions;
+    using System.Collections.Generic;
+    using UnityEngine;
+    using System.Linq;
+
+    public class PlayFabMatchmakingManager : Manager<PlayFabMatchmakingManager>, IRoomProvider
+    {
+        public UnityTask<NetworkConnectionInfo> CreateOrJoinRoom(string roomName)
+        {
+            return UnityTask<NetworkConnectionInfo>.Run(Coroutine());
+
+            IEnumerator<NetworkConnectionInfo> Coroutine()
+            {
+                var enterRoom = CloudFunctionsManager.Instance.Rooms_EnterRoom(roomName);
+
+                // Waiting for enterRoom to finish
+                while (enterRoom.IsCompleted == false)
+                {
+                    yield return default;
+                }
+
+                if (enterRoom.Result.Success == false)
+                {
+                    Debug.LogError($"Failed to Enter Room {roomName}: " + enterRoom.Result.Exception);
+                    yield break;
+                }
+
+                var roomServerInfo = enterRoom.Result.ResultObject;
+
+                string ip = enterRoom.Result.ResultObject.FQDN;
+                int port = enterRoom.Result.ResultObject.Ports.Where(x => x.Name == "game_port").FirstOrDefault().Num;
+
+                if (NetworkingManager.PrintDebugOutput)
+                {
+                    Debug.Log($"Connecting to Sever {ip}, Port = {port}, Room Id = {roomServerInfo.RoomId}, Session Id = {roomServerInfo.SessionId}, Server Id = {roomServerInfo.ServerId}");
+                }
+
+                yield return new NetworkConnectionInfo { Ip = ip, Port = port };
+            }
+        }
+
+        public override void Initialize()
+        {
+            this.SetInstance(this);
+        }
+
+        private MatchmakeRequest GetMatchmakeRequest(GameServerInfo info, bool startNewIfNoneFound)
+        {
+            var request = new MatchmakeRequest
+            {
+                GameMode = info.GameMode,
+                BuildVersion = info.BuildVersion,
+                Region = info.Region,
+                StartNewIfNoneFound = startNewIfNoneFound,
+            };
+
+            if (string.IsNullOrEmpty(info.RoomName) == false)
+            {
+                request.TagFilter = new CollectionFilter
+                {
+                    Includes = new List<Container_Dictionary_String_String>
+                    {
+                        new Container_Dictionary_String_String
+                        {
+                            Data = new Dictionary<string, string>
+                            {
+                                { "Room", info.RoomName.ToUpper() },
+                            },
+                        },
+                    },
+                };
+            }
+
+            return request;
+        }
+
+        public class GameServerInfo
+        {
+            public string GameMode { get; set; }
+
+            public string BuildVersion { get; set; }
+
+            public Region Region { get; set; }
+
+            public string RoomName { get; set; }
+        }
+    }
+}
+
+#endif
---
+++ Runtime/PlayFab/PlayFabUserInfoManager.cs
@@ -0,0 +1,52 @@
+//-----------------------------------------------------------------------
+// <copyright file="PlayFabUserInfoManager.cs" company="Full Circle Games">
+//     Copyright (c) Full Circle Games. All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+#if UNITY
+
+namespace Lost.PlayFab
+{
+    using Lost.Networking;
+
+    public class PlayFabUserInfoManager : Manager<PlayFabUserInfoManager>, IUserInfoManager
+    {
+        public long UserId { get; private set; }
+
+        public string UserHexId { get; private set; }
+
+        public string DisplayName { get; private set; }
+
+        private UserInfo userInfo = new UserInfo();
+
+        public UserInfo GetMyUserInfo()
+        {
+            this.userInfo.UserId = this.UserId;
+            this.userInfo.UserHexId = this.UserHexId;
+            this.userInfo.DisplayName = this.DisplayName;
+
+            userInfo.SetSessionTicket(PlayFabManager.Instance.Login.SessionTicket);
+
+            return userInfo;
+        }
+
+        public override void Initialize()
+        {
+            this.StartCoroutine(Initialize());
+
+            System.Collections.IEnumerator Initialize()
+            {
+                yield return PlayFabManager.WaitForInitialization();
+
+                this.UserId = PlayFabManager.Instance.User.PlayFabNumericId;
+                this.UserHexId = PlayFabManager.Instance.User.PlayFabId;
+                this.DisplayName = PlayFabManager.Instance.User.DisplayName;
+
+                this.SetInstance(this);
+            }
+        }
+    }
+}
+
+#endif
