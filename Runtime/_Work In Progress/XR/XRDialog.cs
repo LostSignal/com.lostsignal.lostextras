a5a8d7653e1d08435d92cde3ac81786a7317613f
Brian Gish <gish.brian@gmail.com>
2022-03-15 11:59:43 -0700

    Fixing another compile bug

--- Runtime/_Work In Progress/XR/XRDialog.cs
+++ Runtime/_Work In Progress/XR/XRDialog.cs
@@ -130,7 +130,11 @@ namespace Lost
         {
             //// TODO [bgish]: Do some calculations to spawn this dialog in front of the user
 
+#if USING_LOST_XR
             this.isPancakeMode = XRManager.IsInitialized == false || XRManager.Instance.IsFlatMode;
+#else
+            this.isPancakeMode = true;
+#endif
 
             if (this.isPancakeMode == false)
             {
